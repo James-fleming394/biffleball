b39c56b00481d7cd4b526deb4836f15b255a19b1
james-fleming394 <james.fleming394@gmail.com>
2025-03-18 11:55:25 -0400

    Router update for new models and controllers

--- app/router.php
+++ app/router.php
@@ -1,13 +1,12 @@
 <?php
-require_once __DIR__ . '/controllers/UserController.php';
-require_once __DIR__ . '/controllers/StandingsController.php';
-require_once __DIR__ . '/controllers/PickController.php';
+require_once 'app/controllers/UserController.php';
+require_once 'app/controllers/StandingsController.php';
+require_once 'app/controllers/PickController.php';
+
 session_start();
 
-// Get the requested page
 $page = $_GET['page'] ?? 'home';
 
-// Routing system
 switch ($page) {
     case 'register':
         UserController::register();
@@ -19,7 +18,7 @@ switch ($page) {
         UserController::logout();
         break;
     case 'profile':
-        if (!isset($_SESSION['user_email'])) {
+        if (!isset($_SESSION['user_id'])) {
             header("Location: /index.php?page=login");
             exit();
         }
@@ -29,14 +28,14 @@ switch ($page) {
         StandingsController::show();
         break;
     case 'pick-team':
-        if (!isset($_SESSION['user_email'])) {
+        if (!isset($_SESSION['user_id'])) {
             header("Location: /index.php?page=login");
             exit();
         }
-        PickController::makePick();
+        PickController::showPicks();
         break;
     default:
-        include __DIR__ . '/views/home.php';
+        include 'app/views/home.php';
         break;
 }
 ?>
