6dd4214b63d07ebf3b13bca4b7140515f88771df
james-fleming394 <james.fleming394@gmail.com>
2025-06-05 10:54:13 -0400

    Update: Newsletter CSS width and height change to 100%

--- app/views/home.php
+++ app/views/home.php
@@ -423,8 +423,8 @@
         display: none;
         position: fixed;
         top: 0; left: 0;
-        width: 100vw;
-        height: 100vh;
+        width: 100%;
+        height: 100%;
         background: rgba(0,0,0,0.7);
         justify-content: center;
         align-items: center;
