66f1f86fef853f6cb1553a46d22ae087434eae04
james-fleming394 <james.fleming394@gmail.com>
2025-06-05 13:11:53 -0400

    Update: Register page html updated to add logo and register form

--- app/views/register.php
+++ app/views/register.php
@@ -1,12 +1,24 @@
 <?php include 'header.php'; ?>
 
-<h2>Register</h2>
+<div class="register-page">
+    <div class="register-logo">
+        <img src="/images/BiffleballSmallLogo.png" alt="BiffleBall Logo">
+    </div>
 
-<form action="index.php?page=register" method="POST">
-    <input type="text" name="username" placeholder="Username" required><br><br>
-    <input type="email" name="email" placeholder="Email" required><br><br>
-    <input type="password" name="password" placeholder="Password" required><br><br>
-    <button type="submit">Register</button>
-</form>
+    <div class="register-box">
+        <h2>Create Your Account</h2>
+        <form action="index.php?page=register" method="POST">
+            <input type="text" name="username" placeholder="Username" required>
+            <input type="email" name="email" placeholder="Email" required>
+            <input type="password" name="password" id="password" placeholder="Password" required>
+            
+            <label>
+                <input type="checkbox" id="togglePassword" style="margin-right: 6px;"> Show Password
+            </label>
+            
+            <button type="submit">Register</button>
+        </form>
+    </div>
+</div>
 
 <?php include 'footer.php'; ?>
