ed954245b6873131d1f50af9a5d5493812f778ea
james-fleming394 <james.fleming394@gmail.com>
2025-06-16 14:44:25 -0400

    Update: Standings Controller updated to incorporate total weekly wins for specifc user.

--- app/controllers/StandingsController.php
+++ app/controllers/StandingsController.php
@@ -3,9 +3,9 @@ require_once __DIR__ . '/../models/Standings.php';
 
 class StandingsController {
     public static function index() {
-        $users = Standings::getAllUsersWithCurrentTeam(); // for the standings page
-        include __DIR__ . '/../views/standings.php';
-    }
+    $users = Standings::getAll(); // get total wins and current team for standings
+    include __DIR__ . '/../views/standings.php';
+}
 
     // Optional: expose getAll for other purposes if needed in controllers
     public static function getAll() {
